89db1eab41d1731f954695d670cd758c437cc4e8
AaronBruce <aaron.bruce@comcast.net>
2017-06-07 06:32:37 -0700

    Completed lab-02 for day-02 and added user stories.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # about-me
 my about me page for code 201d23
+
+As a student, I want to turn in the assignment on time and completed properly so that I get a good grade and keep up with the class.
+
+As a developer, I want to create a clean straightforward program so that other developers can easily read my code. I want the user to be able to run through the website prompts easily.
+
+As a user, I want to be able to run through the website prompts easily and be able to read everything on the site well. I want the web page to look pleasing.
--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-'use strict'
+'use strict';
+
+var userName = prompt('Hi there, user! What is your name?');
+
+var userGreeting = prompt('Hello, ' + userName + '! Are you ready to play a guessing game about me? It will only be 5 questions.');
+  if (userGreeting.trim().toLowerCase() === 'yes', 'y') {
+    alert ('Hurray! Let us get started!');
+  } else  if (userGreeting.trim().toLowerCase() === 'no', 'n') {
+    alert ('Too bad! We are playing anyway!');
+  } else {
+    alert ('I am going to take that as a yes');
+  }
+console.log('User input to prompt userGreeting was ' + userGreeting);
+
+var questionOne = prompt('Do you believe I am holding you here against your will?');
+  if (questionOne.trim().toLowerCase() === 'yes', 'y') {
+    alert ('Hahahaha!! I would never do that to you, friend...?');
+  } else if (questionOne.trim().toLowerCase() == 'no', 'n') {
+    alert ('Wow! You are a really trusting person! :D');
+  } else {
+    alert ('I am also going to take that as a no');
+  }
+console.log('User input to prompt questionOne was ' + questionOne);
+
+
+//Trying something different with the alerts
+var computerResponse;
+var questionTwo = prompt('Do you believe this is a game?');
+  if (questionTwo.trim().toLowerCase() === 'yes', 'y') {
+    computerResponse = 'Ahahahahaha!';
+  } else if (questionTwo.trim().toLowerCase() === 'no', 'n') {
+    computerResponse = '...You are probably right!';
+  } else {
+    computerResponse = 'Too scared to guess, huh?';
+  }
+  console.log('User input to prompt questionTwo was ' + questionTwo);
+
+var questionThree = prompt('Do you feel safe?');
+  if (questionThree.trim().toLowerCase() === 'yes', 'y') {
+    alert ('How quaint :)');
+  } else if (questionThree.trim().toLowerCase() === 'no', 'n') {
+    alert ('You must horribly troubled.');
+  } else {
+    alert ('You are very bad at responding to anything...');
+  }
+console.log('User input to prompt questionThree was ' + questionThree);
+
+var questionFour = prompt('Do you like coffee?');
+  if (questionFour.trim().toLowerCase() === 'yes', 'y') {
+    alert ('Be careful of what you drink!');
+  } else if (questionFour.trim().toLowerCase() === 'no', 'n') {
+    alert ('...bummer...');
+  } else {
+    alert ('...You are not making this very fun, are you.');
+  }
+console.log('User input to prompt questionFour was ' + questionFour);
+
+var questionFive = prompt('Do you like icecream?');
+  if (questionFive.trim().toLowerCase() === 'yes', 'y') {
+    alert ('Ok! Go outside and grab some!');
+  } else if (questionFive.trim().toLowerCase() === 'no', 'n') {
+    alert ('I am going to find you...');
+  } else {
+    alert ('You are boring! I am going to go play with someone else!');
+  }
+console.log('User input to prompt questionFive was ' + questionFive);
+
+var finalResponse;
+  if (finalResponse.trim().toLowerCase() === questionOne + questionTwo + questionThree + questionFour + questionFive) {
+    alert ('Hurray! You answered all the questions!');
+  } else {
+    alert ('You did not answer all of the questions, you butt!');
+  }
